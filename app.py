90b19f286bbc2d8a8f22714b9a42d1acbfe1aeef
Esteban S <eas1797@yahoo.com>
2024-07-15 20:57:03 -0400

    Update app.py
    addition and subtraction

--- app.py
+++ app.py
@@ -17,3 +17,17 @@ print("2. -")
 print("3. *")
 print("4. /")
 
+# Validate input and determine which operation to perform, ask user if they want to enter another equation
+while True:
+    # get user input
+    choice = input("Enter Choice 1/2/3/4: ")
+
+    # check choice
+    if choice == "1":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(num1, " + ", num2, " = ", add(num1, num2))
+    elif choice == "2":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(num1, " - ", num2, " = ", sub(num1, num2))
