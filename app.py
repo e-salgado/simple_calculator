9d53770855f1e0c812d4b526131dce0f55cb5329
Esteban S <eas1797@yahoo.com>
2024-07-15 20:55:21 -0400

    Update app.py
    add return to functions

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 
 # create operation functions
 def add(x, y):
-    x + y
+    return x + y
 def sub(x, y):
-    x - y
+    return x - y
 def mult(x, y):
-    x * y
+    return x * y
 def div(x, y):
-    x / y
+    return x / y
 
 # Display menu choices to user
 print("Choose an Operation")
